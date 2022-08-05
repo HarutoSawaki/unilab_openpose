90d63b3e5722f9d892a351efb72483f94fc513d3
amazing-tomotoshi <tomotoshi.2828@ruri.waseda.jp>
2022-08-05 18:01:45 +0900

    いらないコード削除
    close #1

--- pic.py
+++ pic.py
@@ -1,14 +1,14 @@
 import cv2
 import os
 
-def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, window_name='frame'):
+def save_frame_camera_key(device_num, dir_path, ext='jpg', delay=1, window_name='frame'):
     cap = cv2.VideoCapture(device_num)
 
     if not cap.isOpened():
         return
 
     os.makedirs(dir_path, exist_ok=True)
-    # base_path = os.path.join(dir_path)
+
     paths = ["", "", "", "", ""]
     for num in range(5):
         paths[num] = os.path.join(dir_path, "pose"+str(num+1))
@@ -41,4 +41,4 @@ def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, wi
     cv2.destroyWindow(window_name)
 
 
-save_frame_camera_key(0, 'picture', 'pose')
+save_frame_camera_key(0, 'picture')
